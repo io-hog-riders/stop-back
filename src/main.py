76698868d626e9d2adc5ab01e34ad30848c7691a
Jakub Wiecek <Jakub.Wiecek@ibm.com>
2026-04-09 17:02:52 +0200

    fix the module structure and imports

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 OSRM_URL = "https://router.project-osrm.org"
 
-from src.routes.plan.router_mock import router as plan_router_mock
-from src.routes.stops.router_mock import router as stops_router_mock
+from routes.plan.router_mock import router as plan_router_mock
+from routes.stops.router_mock import router as stops_router_mock
 
 app.include_router(plan_router_mock)
 app.include_router(stops_router_mock)
